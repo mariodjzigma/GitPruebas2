edd742a10e4e1ac3b1a5d7c0a50048c9002917fd
Mario <mariohernandez28@outlook.com>
2014-03-21 19:31:07 -0600

    fomenta

---
+++ Pk2_RSVPwapp/Pk2_RSVPwapp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pk2_RSVPwapp.Models;
+
+namespace Pk2_RSVPwapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        
+        public ActionResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ?
+                "Buenos Dias !!!" : "Buenas Tardes";
+            return View();
+        }
+        //Get : /Home/RsvpForm
+        //Redenderea el Link de la forma del RSVP
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        //Responde a un Post: /Home/RsvpForm
+        [HttpPost]
+        public ViewResult RsvpForm(GuestReponse guestResponse)
+        {
+            //verifica errores de validacion
+            if (ModelState.IsValid)
+            {
+                //Todo: Enviar respuesta al correo de organizador
+                return View("Agradecimientos", guestResponse);
+            }
+            else
+            {
+                return View();
+
+            }
+        }
+    }
+
+}
---
+++ Pk2_RSVPwapp/Pk2_RSVPwapp/Models/GuestReponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//usado para validar campos
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Pk2_RSVPwapp.Models
+{
+    public class GuestReponse
+    {   
+        [Required(ErrorMessage= "Por favor ingresa su nombre:")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage= "Por favor ingrese su correo electronico.")]
+        [RegularExpression(".+\\@..+",
+            ErrorMessage= "Favor de ingresar una cuenta de correo valida")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage= "Por favor ingrese su numero telefonico.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Por favor especifique si asistira o no.")]
+        public bool? WillAttend { get; set; }
+    }
+}
