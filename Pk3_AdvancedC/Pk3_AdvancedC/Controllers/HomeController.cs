885e717164be1a282f25c225001e4ae60efaeadc
Mario <mariohernandez28@outlook.com>
2014-03-21 19:33:09 -0600

    Advancedc#

---
+++ Pk3_AdvancedC/Pk3_AdvancedC/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pk3_AdvancedC.Models;
+
+namespace Pk3_AdvancedC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public string Index()
+        {
+            return "Navegar a una URL para mostrar un ejemplo";
+        }
+
+        public ViewResult AutoProperty();
+        ProductID = 12,
+        Name = "kayak",
+        Description =  "Kiosteca Blue",
+        Category = "SPORTS",
+        Price = 12.5
+
+        { 
+
+            
+        //Crear un nuevo objeto
+            //De la clase producto
+           /* Product myProduct  = new Product();
+
+            //Establece el vlor de la propiedad
+            myProduct.ProductID = 12;
+            myProduct.Name = "Kayak";
+            myProduct.Description = "kiosteca blue";
+            myProduct.Category = "SPORT";
+            myProduct.Price = 12.5F;
+
+            string salida =
+                String.Format("{0}-{1}-{2}-{3}-{4:C0}",
+                myProduct.ProductID,
+                 myProduct.Name,
+                 myProduct.Description,
+                 myProduct.Category,
+                 myProduct.Price);
+
+            //obtiene la propiedad
+            //string productsName = myProducts.Name;
+
+            //genera la vista
+            return View("Result",
+               (object)salida);
+            
+        
+        }
+
+    }
+}
---
+++ Pk3_AdvancedC/Pk3_AdvancedC/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pk3_AdvacedCsharp.Models
+{
+    public class Product
+    {
+        // private string name;
+        // public string Name {
+        //   get { return name; }
+
+        //set { name = value; }
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string Category { get; set; }
+    }
+}
