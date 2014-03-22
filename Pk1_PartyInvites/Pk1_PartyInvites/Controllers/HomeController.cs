7b16a20f0f8470de2e78d3c2cc36fcd53b559280
Mario <mariohernandez28@outlook.com>
2014-03-21 19:28:05 -0600

    RSVPwapp

---
+++ Pk1_PartyInvites/Pk1_PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pk1_PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            var segundos = DateTime.Now.Second;
+            var minutos = DateTime.Now.Minute;
+            ViewBag.Dias = hour < 12 ? "Buenos Dias !!" : "Buenas Tardes!!";
+            ViewBag.Horas = hour;
+            ViewBag.Minutos = minutos;
+            ViewBag.Segundos = segundos;
+            return View();
+        }
+
+    }
+}
+
