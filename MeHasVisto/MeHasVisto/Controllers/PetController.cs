5ced838ba9dd0711694a4dfa40ffe7cdb0ceaefc
Mario <mariohernandez28@outlook.com>
2014-03-21 19:40:07 -0600

    Visto

---
+++ MeHasVisto/MeHasVisto/Controllers/PetController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeHasVisto.Controllers
+{
+    public class PetController : Controller
+    {
+        //
+        // GET: /Pet/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Display()
+        {
+            var name = (string)RouteData.Values["id"];
+            var model = PetManagemet.GetByBame(name);
+            if (model == null)
+                return RedirectToAction("NotFound");
+            return View(model);
+        }
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+        public FileResult DownLoadPicture()
+        {
+            var name = (string)RouteData.Values["id"];
+            var picture = "content/Uploads" + ".jpg";
+            var contentType = "image/jpg";
+            var fileName = name = ".jpg";
+            return File(picture, contentType, fileName);
+        }
+    }
+}
